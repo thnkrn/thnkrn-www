fabd9e78a17d728add404f6d9cdb21894d6cf908
Thanakorn <thanakorn.ariyagusolsuthi@prudential.co.th>
2021-06-05 13:16:31 +0700

    fix(playground): remove temp value

--- src/pages/Playground/index.tsx
+++ src/pages/Playground/index.tsx
@@ -9,41 +9,6 @@ const Playground = () => (
   <Layout noFooter>
     <H1>Playground!</H1>
     <span>Welcome to my playground</span>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
-    <div>1234</div>
   </Layout>
 )
 
