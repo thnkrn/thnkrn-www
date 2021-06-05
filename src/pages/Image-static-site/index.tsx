6ff89fcbfad6f59c83b0aa1a90cf196c32bcd3b3
Thanakorn <thanakorn.ariyagusolsuthi@prudential.co.th>
2021-06-05 15:10:54 +0700

    build: cleanup page

--- src/pages/Image-static-site/index.tsx
+++ src/pages/Image-static-site/index.tsx
@@ -1,17 +1,11 @@
-import { styled } from '@compiled/react'
 import ImageWrapper from 'components/ImageWrapper'
 import Layout from 'components/Layout'
+import TYPO from 'constants/typography'
 import Img from 'react-optimized-image'
-
-const H1 = styled.h1`
-  font-weight: bold;
-`
 
 const IndexPage = () => (
   <Layout noHeader noFooter>
-    <H1>
-      <a href="https://github.com/cyrilwanner/next-optimized-images">next optimized images</a>
-    </H1>
+    <h1 className={TYPO.h1}>Image Static Site</h1>
     <ImageWrapper width={1920} height={1080}>
       <Img
         css={{
