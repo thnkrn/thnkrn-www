e7c2c352b41444e711eb0632aa8bbdaaa95941a2
Thanakorn <thanakorn.ariyagusolsuthi@prudential.co.th>
2021-06-06 14:35:29 +0700

    style: add font swap

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -21,7 +21,10 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
           />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons&display=swap"
+          />
           <noscript>
             <link
               rel="stylesheet"
