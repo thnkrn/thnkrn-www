9fb9dec77dbc87634ee76e935ee934645921741c
Thanakorn <thanakorn.ariyagusolsuthi@prudential.co.th>
2021-06-11 12:27:57 +0700

    fix(info): edit content

--- src/components/Info/index.tsx
+++ src/components/Info/index.tsx
@@ -39,13 +39,13 @@ const Info = () => (
         And now as a Fullstack Developer, I'm also working on the Backend stuff, and Database too.
       </span>
       <span className={TYPO.body2}>
-        I'm pay attention the most on developer experience like write clean, composable, and
-        maintainable code as world class products for both internal and external customer. Also, I'm
+        I'm paying attention the most to developer experience like writing clean, composable, and
+        maintainable code as world-class products for both internal and external customers. Also, I
         really care about the processes, high quality of code and products, and aesthetics.
       </span>
       <span className={TYPO.body2}>
-        Moreover, I'm a tech-savvy guy, who is always curious and excite about new technology stacks
-        I'm also like to learn, listen, and discuss about the tech things with my teammates.
+        Moreover, I'm a tech-savvy guy, who is always curious and excite about new technology
+        stacks. I'm also like to learn, listen, and discuss tech things with my teammates.
       </span>
       <span className={TYPO.body2}>
         Lastly, I'm a fast learner and open to every avenue that comes to me. And I'm eager to learn
